f90d113b79c9d1cb0f735e8af6da447213630850
Vasiliy Cherniatyn <v.v.4ern9tin@gmail.com>
2016-04-18 12:09:48 +0300

    HomeProj

---
+++ AsyncHomeProject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncHomeProject {
+
+	class Program {
+		static void Main(string[] args) {
+			CancellationTokenSource cts = new CancellationTokenSource();
+			double firstElem = 4;
+			double secondElem = 3;
+			var rsultMulAsync = TaskExample.MulTaskAsynk(firstElem, secondElem, cts.Token);
+
+			var secondRes = rsultMulAsync.ContinueWith(task => {
+				var res = TaskExample.Mul(task.Result, task.Result);
+				Console.WriteLine($"Mul {firstElem} * {secondElem} = {task.Result}");
+				Console.WriteLine($"MulContinue {task.Result} * {task.Result} = {res}");
+			}, TaskContinuationOptions.OnlyOnRanToCompletion);
+
+			var thirdRes = rsultMulAsync.ContinueWith(task => Console.WriteLine("Operation was canceled!"),
+													  TaskContinuationOptions.OnlyOnCanceled);
+
+			Task.Run(() => {
+				Thread.Sleep(1500);
+				cts.Cancel();
+			});
+			Console.ReadLine();
+		}
+	}
+}
---
+++ AsyncHomeProject/TaskExample.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncHomeProject
+{
+    class TaskExample
+    {
+        public static async Task<double> MulTaskAsynk(double firstElem, double secondElem, CancellationToken token)
+        {
+            int count = 5;
+            var result = await Task.Run(() => Mul(firstElem, secondElem));
+            while (count-- > 0)
+            {
+                token.ThrowIfCancellationRequested();
+                Thread.Sleep(500);
+            }
+            return result;
+        }
+        public static double Mul(double firstElem, double secondElem)
+        {
+            return firstElem * secondElem;
+        }
+    }
+}
